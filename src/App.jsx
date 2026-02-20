05167944f395bb861dc47f231932a4c16994be71
mohitkushwah14032000 <mk7514004@gmail.com>
2026-02-21 00:19:19 +0530

    crud

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,28 @@ import Counter from './Components/Counter'
 import Fetchapi from './Components/Fetchapi'
 import Home from '../Home'
 import Asyncawait from './Components/Asyncawait'
-import React, { Suspense } from 'react'
+import React, { Suspense, useState } from 'react'
+import Apicrud from './Components/Apicrud'
 
 const Lazyloading = React.lazy(() => import('./Components/Lazyloading'))
+const Loadcard = React.lazy(() => import('./Components/Loadcard'))
 
 function App() {
 
+  const Load = () => {
+    const [show, setShow] = useState(false)
+    return (
+      <div className='text-center'>
+        <h1>Learn Lazy Loading</h1>
+        <button onClick={() => setShow(true)}>
+          Load Content
+        </button>
+        {show && <Suspense fallback={<h2>Loading...</h2>}>
+          <Loadcard />
+        </Suspense>}
+      </div>
+    )
+  }
 
   return (
     <>
@@ -21,10 +37,12 @@ function App() {
             <Route path='counter' element={<Counter />}></Route>
             <Route path='fetchapi' element={<Fetchapi />}></Route>
             <Route path='asyncawait' element={<Asyncawait />}></Route>
+            <React path='apicrud' element={<Apicrud />}></React>
+            <Route path='card' element={<Load />}></Route>
             <Route path='load' element={<Suspense fallback={<h1 className='text-center text-[1.3rem]'>Loading...</h1>}>
               <div className='text-center text-[1.5rem]'>
                 <h1 className='font-bold'>Learn Loading</h1>
-                <Lazyloading/>
+                <Lazyloading />
               </div>
             </Suspense>}>
             </Route>
---
+++ src/Components/Loadcard.jsx
@@ -0,0 +1,11 @@
+const Loadcard = () => {
+    return (
+        <div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit...
+            </p>
+        </div>
+    )
+}
+
+export default Loadcard;
